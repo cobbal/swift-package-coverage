408162062ab07155e019bee77cbc5597f52793a5
Braden Scothern <BradenScothern@gmail.com>
2021-06-08 15:00:57 -0600

    Fixing action.sh so it will hopefully work

--- action.sh
+++ action.sh
@@ -1,4 +1,7 @@
 EXECUTABLE=.build/release/package-coverage
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd ${SCRIPT_DIR}
 
 make build-release
 ${EXECUTABLE} --version
