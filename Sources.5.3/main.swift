03b06552f96b5f53acf33e0429fb286cb3c30f47
Braden Scothern <BradenScothern@gmail.com>
2021-07-01 21:40:12 -0600

    Added an error if you try to build the Sources5.3/main.swift file when your system has Swift 5.4 available

--- Sources.5.3/main.swift
+++ Sources.5.3/main.swift
@@ -1,3 +1,7 @@
 import PackageCoverage
 
+#if swift(>=5.4)
+#error("Swift 5.3 use of main.swift is deprecated in favor of @main on systems that support Swift 5.4+")
+#endif
+
 SwiftPackageCoverageCommand.main()
